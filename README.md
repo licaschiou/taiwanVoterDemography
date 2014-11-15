7d9908e23fc362665d6436959f7258928387b24e
Licas <licaschiou@gmail.com>
2014-11-15 17:51:42 +0800

    1. Seperate function for tidy data set and g0v format 2. Modify output filename for g0v format 3. Add exploratory example for tidy data set

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ Taiwan Voter Demography
 Goals :   
 1. Transform demography data from MOI Taiwan into g0v format.  
 2. Transofrm demography data from MOI Taiwan into tidy data set.  
-   (Tidy data http://ramnathv.github.io/pycon2014-r/explore/tidy.html)  
+   (Tidy data http://ramnathv.github.io/pycon2014-r/explore/tidy.html)
+
+Requirement :   
+1. R, R.Studio or other compatible R IDE.
+2. R package : tidyr, dplyr, 
 
 Usage :  
 1. Go to http://data.moi.gov.tw/MoiOD/Data/DataDetail.aspx?oid=F4478CE5-7A72-4B14-B91A-F4701758328F
@@ -13,8 +17,13 @@ Usage :
 2. Unzip data set into a folder.
 3. Put tidyDemography.R in the same folder.
 4. Start R or R.Studio.
-5. Use source("tidyDemography.R", encoding="UTF-8") to load script.
-6. Excute processFileList(dataFileList) to batch process all dataset in the folder.
+5. To load script: source("tidyDemography.R", encoding="UTF-8")
+6. To generate demography in g0v format : g0vDemo().  
+   This script will automatically detect csv files in the same folder and processes all of them.  
+   Please make sure there are only legitimite csv files in the same folder.
+7. To generate tidy demography: regionDemo <- tidyDemo(regionDemoFileName).  
+   This function will return a data set for exploratory data analysis.
 
 Todo : 
 1. codebook.md
+2. 縣市輸出檔名
--- tidyDemography.R
+++ tidyDemography.R
@@ -1,36 +1,131 @@
-dataFileList <- list.files(".")
-dataFileList <- dataFileList[grep("csv$", dataFileList)]
-##processFileList(dataFileList)
-processFileList <- function(fileList = NULL){
-	for(file in fileList){
+library(tidyr)
+library(dplyr)
+
+tidyDemo <- function(file = NULL){
+	raw <- readLines(file, encoding="UTF-8")
+	raw2 <- read.csv(textConnection(paste0(raw, collapse="\n")),  header=FALSE, stringsAsFactors=FALSE)
+
+	variableName <- raw2[1, 1:209]
+	variableName[1,1] <- "統計年月"
+	variableName[5] <- "總人口數"
+	variableName[6] <- "總人口數男"
+	variableName[7] <- "總人口數女"
+	variableName <- sub("-", ".", variableName)
+
+	names(raw2) <- variableName
+	raw2 <- raw2[2:dim(raw2)[1], 1:dim(raw2)[2]-1]	
+	raw2 <- raw2[(!is.na(raw2$統計年月)),]
+	
+	tidier <- raw2 %>% gather(
+			sexage, 
+			population, 
+			-統計年月, 
+			-區域別, 
+			-村里, 
+			-戶數, 
+			-總人口數, 
+			-總人口數男, 
+			-總人口數女
+		)
+
+	tidyAll <- tidier %>% separate(sexage, into = c("年齡", "性別"), sep = "\\.") 		
+	tidyAll$年齡 <- sub("歲", "", tidyAll$年齡)
+	tidyAll$年齡 <- sub("以上", "", tidyAll$年齡)		
+	tidyAll[,c(1,4,5,6,7,8,10)] <- as.numeric(unlist(tidyAll[,c(1,4,5,6,7,8,10)]))
+	### tidyAll <- tidyAll[(!is.na(tidyAll$統計年月)),]
+	tidyVoter <- tidyAll[(tidyAll$年齡 >= 20),]
+
+	splitedChar <- ""
+	if(grepl("縣", tidyAll$區域別[1])){
+		splitedChar <- "縣"
+	}else if(grepl("市", tidyAll$區域別[1])){
+		splitedChar <- "市"
+	}
+	cityName <- unlist(strsplit(tidyAll$區域別[1], "市|縣", fixed = FALSE))[1]
+	cityName <- paste(cityName, splitedChar, sep="")
+	tidyVoterZone <- data.frame(
+		統計年月=tidyAll$統計年月[1], 
+		區域別=cityName, 
+		性別=rep(c("男","女"), each=81), 
+		年齡=rep(c(20:100), 2), 
+		人口=0)	
+
+	for(age in 20:100){
+		ageSubSet <- tidyAll[(tidyAll$年齡 == age & tidyAll$性別 == "男"),]$population
+		tidyVoterZone[(tidyVoterZone$年齡 == age & tidyVoterZone$性別 == "男"),]$人口 <- sum(ageSubSet)
+		ageSubSet <- tidyAll[(tidyAll$年齡 == age & tidyAll$性別 == "女"),]$population
+		tidyVoterZone[(tidyVoterZone$年齡 == age & tidyVoterZone$性別 == "女"),]$人口 <- sum(ageSubSet)
+	}
+	tidyVoterZone
+
+	# Exploratory example : 
+	# tidyVoterZoneMale <- tidyVoterZone[(tidyVoterZone$性別 == "男"),]
+	# tidyVoterZonefemale <- tidyVoterZone[(tidyVoterZone$性別 == "女"),]
+
+	# cumPopuplation <- cumsum(tidyVoterZoneMale$人口)
+	# sumPopulation <- sum((tidyVoterZoneMale$人口))
+	# halfMaleVoter <- tidyVoterZoneMale$年齡[length(cumPopuplation[(cumPopuplation <= sumPopulation*0.5)])]
+	# cumPopuplation <- cumsum(tidyVoterZonefemale$人口)
+	# sumPopulation <- sum((tidyVoterZonefemale$人口))
+	# halfFemaleVoter <- tidyVoterZonefemale$年齡[length(cumPopuplation[(cumPopuplation <= sumPopulation*0.5)])]
+	
+	# halfLabelMale <- paste("50%男性小於", halfMaleVoter, "歲", sep="")
+	# halfLabelFemale <- paste("50%女性小於", halfFemaleVoter, "歲", sep="")
+	# plotTitle <- paste(cityName, "投票人口年齡分布", sep="")
+
+	# png(file = "demographyLine.png", bg = "white", width = 720, height = 480)
+	# ggplot(tidyVoterZone, aes(x=年齡, y=人口)) + 
+	#     geom_line(aes(colour = 性別)) + 
+	#     labs(title=plotTitle) +
+	#     geom_vline(xintercept = c(halfMaleVoter, halfFemaleVoter)) + 
+	#     annotate("text", label = c(halfLabelMale, halfLabelFemale), x = c(56,56), y = c(5000,10000), size = 4, colour = "red")
+	# dev.off()
+	
+	# outputDate <- as.character(raw5$統計年月[1])
+	# outputFileName <- raw5$區域別[1]
+	# splitedChar <- ""
+	# if(grepl("縣", outputFileName)){
+	# 	splitedChar <- "縣"
+	# }else if(grepl("市", outputFileName)){
+	# 	splitedChar <- "市"
+	# }
+	# cityName <- unlist(strsplit(outputFileName, "市|縣", fixed = FALSE))[1]
+	# fileName <- paste(outputDate, cityName, splitedChar, "人口資料tidy.csv", sep="")
+	# write.csv(tidyVoterZone, file = fileName, row.names=FALSE, fileEncoding="UTF-8")
+}
+
+g0vDemo <- function(){
+	dataFileList <- list.files(".")
+	dataFileList <- dataFileList[grep("csv$", dataFileList)]
+	for(file in dataFileList){
 		raw <- readLines(file, encoding="UTF-8")
 		raw2 <- read.csv(textConnection(paste0(raw, collapse="\n")),  header=FALSE, stringsAsFactors=FALSE)
 
-		variableName <- raw2[1,]
-		variableName <- variableName[,1:209]
-		raw3 <- raw2[2:457,]
-		raw3 <- raw3[,1:209]
-
+		variableName <- raw2[1, 1:209]
 		variableName[1,1] <- "統計年月"
-		names(raw3) <- variableName[1,]
+		variableName[5] <- "總人口數"
+		variableName[6] <- "總人口數男"
+		variableName[7] <- "總人口數女"
+		variableName <- sub("-", ".", variableName)
 
-		raw4 <- raw3[,c(1:7, 48:209)]
-		raw4 <- raw3[,c(1:7, 48:209)]
+		names(raw2) <- variableName
+		raw2 <- raw2[2:dim(raw2)[1], 1:dim(raw2)[2]-1]	
+		raw2 <- raw2[(!is.na(raw2$統計年月)),]
 
-		raw4[,c(8:169)] <- lapply(raw4[,c(8:169)], as.numeric)
+		raw4 <- raw2[,c(8:dim(raw2)[2]-1)]
+		raw4[,c(1:202)] <- lapply(raw4[,c(1:202)], as.numeric)
 
-		raw5 <- raw4[,c(1:7)]
-
-		raw5$M20_34 <- rowSums(raw4[,seq(from = 8, to = 37, by = 2)])
-		raw5$F20_34 <- rowSums(raw4[,seq(from = 9, to = 38, by = 2)])
-		raw5$M35_49 <- rowSums(raw4[,seq(from = 38, to = 66, by = 2)])
-		raw5$F35_49 <- rowSums(raw4[,seq(from = 39, to = 68, by = 2)])
-		raw5$M50_64 <- rowSums(raw4[,seq(from = 68, to = 96, by = 2)])
-		raw5$F50_64 <- rowSums(raw4[,seq(from = 69, to = 98, by = 2)])
-		raw5$M65_79 <- rowSums(raw4[,seq(from = 98, to = 126, by = 2)])
-		raw5$F65_79 <- rowSums(raw4[,seq(from = 99, to = 128, by = 2)])
-		raw5$M80up <- rowSums(raw4[,seq(from = 128, to = 168, by = 2)])
-		raw5$F80up <- rowSums(raw4[,seq(from = 129, to = 169, by = 2)])
+		raw5 <- raw2[,c(1:7)]
+		raw5$M20_34 <- rowSums(raw4[,seq(from = 20*2+1, to = 34*2+1, by = 2)])
+		raw5$F20_34 <- rowSums(raw4[,seq(from = 20*2+2, to = 34*2+2, by = 2)])
+		raw5$M35_49 <- rowSums(raw4[,seq(from = 35*2+1, to = 49*2+1, by = 2)])
+		raw5$F35_49 <- rowSums(raw4[,seq(from = 35*2+2, to = 49*2+2, by = 2)])
+		raw5$M50_64 <- rowSums(raw4[,seq(from = 50*2+1, to = 64*2+1, by = 2)])
+		raw5$F50_64 <- rowSums(raw4[,seq(from = 50*2+2, to = 64*2+2, by = 2)])
+		raw5$M65_79 <- rowSums(raw4[,seq(from = 65*2+1, to = 79*2+1, by = 2)])
+		raw5$F65_79 <- rowSums(raw4[,seq(from = 65*2+2, to = 79*2+2, by = 2)])
+		raw5$M80up <- rowSums(raw4[,seq(from = 80*2+1, to = 100*2+1, by = 2)])
+		raw5$F80up <- rowSums(raw4[,seq(from = 80*2+2, to = 100*2+2, by = 2)])
 
 		raw5$投票人口男 <- rowSums(raw5[, grep("M", names(raw5), value=TRUE)])
 		raw5$投票人口女 <- rowSums(raw5[, grep("F", names(raw5), value=TRUE)])
@@ -38,61 +133,13 @@ processFileList <- function(fileList = NULL){
 		outputDate <- as.character(raw5$統計年月[1])
 		outputFileName <- raw5$區域別[1]
 		splitedChar <- ""
-		if(grepl("市", outputFileName)){
-			splitedChar <- "市"
-		}else if(grepl("縣", outputFileName)){
+		if(grepl("縣", outputFileName)){
 			splitedChar <- "縣"
+		}else if(grepl("市", outputFileName)){
+			splitedChar <- "市"
 		}
 		cityName <- unlist(strsplit(outputFileName, "市|縣", fixed = FALSE))[1]
 		fileName <- paste(outputDate, cityName, splitedChar, "人口資料.csv", sep="")
 		write.csv(raw5, file = fileName, row.names=FALSE, fileEncoding="UTF-8")
 	}
-
 }
-
-processFile <- function(file = NULL){
-	raw <- readLines(file, encoding="UTF-8")
-	raw2 <- read.csv(textConnection(paste0(raw, collapse="\n")),  header=FALSE, stringsAsFactors=FALSE)
-
-	variableName <- raw2[1,]
-	variableName <- variableName[,1:209]
-	raw3 <- raw2[2:457,]
-	raw3 <- raw3[,1:209]
-
-	variableName[1,1] <- "統計年月"
-	names(raw3) <- variableName[1,]
-
-	raw4 <- raw3[,c(1:7, 48:209)]
-	raw4 <- raw3[,c(1:7, 48:209)]
-
-	raw4[,c(8:169)] <- lapply(raw4[,c(8:169)], as.numeric)
-
-	raw5 <- raw4[,c(1:7)]
-
-	raw5$M20_34 <- rowSums(raw4[,seq(from = 8, to = 37, by = 2)])
-	raw5$F20_34 <- rowSums(raw4[,seq(from = 9, to = 38, by = 2)])
-	raw5$M35_49 <- rowSums(raw4[,seq(from = 38, to = 66, by = 2)])
-	raw5$F35_49 <- rowSums(raw4[,seq(from = 39, to = 68, by = 2)])
-	raw5$M50_64 <- rowSums(raw4[,seq(from = 68, to = 96, by = 2)])
-	raw5$F50_64 <- rowSums(raw4[,seq(from = 69, to = 98, by = 2)])
-	raw5$M65_79 <- rowSums(raw4[,seq(from = 98, to = 126, by = 2)])
-	raw5$F65_79 <- rowSums(raw4[,seq(from = 99, to = 128, by = 2)])
-	raw5$M80up <- rowSums(raw4[,seq(from = 128, to = 168, by = 2)])
-	raw5$F80up <- rowSums(raw4[,seq(from = 129, to = 169, by = 2)])
-
-	raw5$投票人口男 <- rowSums(raw5[, grep("M", names(raw5), value=TRUE)])
-	raw5$投票人口女 <- rowSums(raw5[, grep("F", names(raw5), value=TRUE)])
-
-	outputDate <- as.character(raw5$統計年月[1])
-	outputFileName <- raw5$區域別[1]
-	splitedChar <- ""
-	if(grepl("市", outputFileName)){
-		splitedChar <- "市"
-	}else if(grepl("縣", outputFileName)){
-		splitedChar <- "縣"
-	}
-	cityName <- unlist(strsplit(outputFileName, "市|縣", fixed = FALSE))[1]
-	fileName <- paste(outputDate, cityName, splitedChar, "人口資料g0v.csv", sep="")
-	write.csv(raw5, file = fileName, row.names=FALSE, fileEncoding="UTF-8")
-}
-
